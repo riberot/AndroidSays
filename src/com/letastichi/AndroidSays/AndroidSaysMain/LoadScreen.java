a0c7d3cfde085f05c0e8a6bfad815b0f6d617c88
Daniel Houk <daniel@ninecollective.com>
2013-06-10 13:57:28 -0500

    The app is now working and showing the main page.

--- src/com/letastichi/AndroidSays/AndroidSaysMain/LoadScreen.java
+++ src/com/letastichi/AndroidSays/AndroidSaysMain/LoadScreen.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import com.letastichi.AndroidSays.R;
 
+import java.util.logging.Handler;
+
 public class LoadScreen extends Activity {
     /**
      * Called when the activity is first created.
@@ -12,9 +14,11 @@ public class LoadScreen extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.load_screen);
 
-        Intent intent = new Intent(getApplicationContext(), MainMenu.class);
-        startActivity(intent);
+        if(savedInstanceState == null){
+            Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+            startActivity(intent);
+        }
     }
 }
